f1bfd4039d368685e91aa90decbb3f709b2a4f74
YuhangQ <i@yuhangq.com>
2021-09-22 16:59:28 +0800

    新增支持Java、Pascal、C、Python语言支持

--- scripts/build_in_sandbox.py
+++ scripts/build_in_sandbox.py
@@ -21,6 +21,27 @@ if lang == 'C++':
         res = os.system('g++ 2>compile.result -x c++ answer.code implementer.cpp -o answer')
     else:
         res = os.system('g++ 2>compile.result -x c++ answer.code -o answer')
+if lang == 'C':
+    if('withImplementer' in sys.argv):
+        res = os.system('gcc 2>compile.result -x c answer.code implementer.c -o answer')
+    else:
+        res = os.system('gcc 2>compile.result -x c answer.code -o answer')
+if lang == 'Pascal':
+    res = os.system('fpc 2>compile.result answer.code -O2')
+
+import re
+
+if lang == 'Java8':
+    content = open("answer.code").read()
+    name = re.search("class (.*)\\b", content).groups()[0]
+    os.system('mv answer.code ' + str(name) + '.java')
+    os.system('/usr/lib/jvm/java-8-openjdk-amd64/bin/javac ' + str(name) + '.java')
+
+if lang == 'Java11':
+    content = open("answer.code").read()
+    name = re.search("class (.*)\\b", content).groups()[0]
+    os.system('mv answer.code ' + str(name) + '.java')
+    os.system('/usr/lib/jvm/java-11-openjdk-amd64/bin/javac ' + str(name) + '.java')
 
 if res != 0: res = -1
 exit(res)
--- scripts/judge_in_sandbox.py
+++ scripts/judge_in_sandbox.py
@@ -19,12 +19,24 @@ os.system('rm -rf answer.result')
 inputfile = '../input/' + sys.argv[1]
 outputfile = 'answer.result'
 
-if lang == 'C++' or lang == 'C' or lang == 'C++11':
+if lang == 'C++' or lang == 'C' or lang == 'C++11' or lang == 'Pascal':
     res = os.system('./answer < %s > %s ' % (inputfile, outputfile))
 if lang == 'Python3':
     res = os.system('python3 ./answer.code < %s > %s ' % (inputfile, outputfile))
 if lang == 'Python2':
     res = os.system('python2 ./answer.code < %s > %s ' % (inputfile, outputfile))
+
+if lang == 'Java8':
+    for file in os.listdir('.'):
+        if file.endswith('.class'):
+            os.system('/usr/lib/jvm/java-8-openjdk-amd64/bin/java %s -Xmx1024m -Xss1024m < %s > %s ' % (file[:-6], inputfile, outputfile))
+
+
+if lang == 'Java11':
+    for file in os.listdir('.'):
+        if file.endswith('.class'):
+            os.system('/usr/lib/jvm/java-11-openjdk-amd64/bin/java %s -Xmx1024m -Xss1024m < %s > %s ' % (file[:-6], inputfile, outputfile))
+
 
 if res != 0: res = -1
 exit(res)
