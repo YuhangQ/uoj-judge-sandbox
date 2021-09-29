2ef6bafb33617e7b223bc148bf2e11a07aff215c
YuhangQ <i@yuhangq.com>
2021-09-29 10:31:31 +0800

    增加docker镜像build

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,6 @@
 
-let conf = {
-    uoj_protocol: "http",
-    uoj_host: "127.0.0.1:8080",
-    judger_name: "sandbox",
-    judger_password: "ymQGFPl7vPRCmWOOXnvrLexnj5qduKw0",
-    socket_port: 2333,
-    socket_password: "dNdMZey5xi7NTBdSBBw8LIbC4XZFVlMC"
-}
+import * as fs from "fs"
+
+let conf = JSON.parse(fs.readFileSync(__dirname + "../../../.conf.json").toString())
 
 export { conf };
