9c172d453a80d48d822044075a1f76c4d2163926
YuhangQ <i@yuhangq.com>
2021-09-29 10:38:09 +0800

    修改docker

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 
 import * as fs from "fs"
 
-let conf = JSON.parse(fs.readFileSync(__dirname + "../../../.conf.json").toString())
+let conf = JSON.parse(fs.readFileSync(__dirname + "/../../../.conf.json").toString())
 
 export { conf };
